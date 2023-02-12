02e01f81bffc152fa8fac635e49312502eeb62de
eohara97 <95063473+eohara97@users.noreply.github.com>
2023-02-11 20:20:19 -0500

    Added Web View widget on activity_second to trivago

---
+++ SecondActivity.java
@@ -0,0 +1,48 @@
+package com.example.tripplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.ViewGroup;
+import android.webkit.WebView;      // need this for WebView
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button addButton;
+    private Button continueButton;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        webView = findViewById(R.id.webview);
+        webView.loadUrl("https://www.trivago.ca/");
+
+        continueButton = findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openThirdActivity();
+            }
+        });
+
+    }
+
+
+
+    public void openThirdActivity() {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+    }
+}
